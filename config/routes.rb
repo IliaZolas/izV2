d53d47c5561d9e1a44b3cf3aa89f466dcfb93ced
Ilia Zolas <ilia@thedmcsa.co.za>
2021-08-22 23:03:04 +0200

    navbar-dashboard-controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  unauthenticated :user do
+    devise_scope :user do
+      get '/' => 'pages#home'
+      # get "/about" => "pages#about"
+      # get "/contact" => "pages#contact"
+    end
+  end
+
+  authenticated :user do
+    root 'dashboard#index'
+
+  end
 end
