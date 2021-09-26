7990a0c24f0c65a461222e498942b2ee8655e1f2
Ilia Zolas <ilia@thedmcsa.co.za>
2021-09-26 17:53:43 +0200

    link previews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,3 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   helper ActionText::Engine.helpers
 end
+
+def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+end
