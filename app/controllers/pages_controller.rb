6c68d79320587c17e99ca91e42a3f0a3b768a3fb
Ilia Zolas <ilia@thedmcsa.co.za>
2021-09-27 18:46:29 +0200

    tric bttn colors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @blogposts = Blogpost.all
+    @blogposts = Blogpost.all.order("updated_at DESC")
     @employments = Employment.all
     @portfolios = Portfolio.all
     @projects = Project.all
